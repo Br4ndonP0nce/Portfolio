86a020e7acc70d700aff5ceee6879b0cced97cd8
Br4ndonP0nce <brandoncompany876@gmail.com>
2024-12-29 21:15:57 -0600

    updated typo in tailwind config

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -57,7 +57,7 @@ const NavBar = () => {
         <nav className="flex size-full items-center justify-between p-4">
           {/* Logo and Product button */}
           <div className="flex items-center gap-7">
-            <img src="/images/logo.png" alt="logo" className="w-10" />
+            <img src="/images/Logo.png" alt="logo" className="w-10" />
 
             <Button
               id="product-button"

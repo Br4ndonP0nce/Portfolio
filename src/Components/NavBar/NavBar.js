9982505052b375fa4e814457b0e99dde7d06fc42
Br4ndonP0nce <brandoncompany876@gmail.com>
2024-12-30 19:18:57 -0600

    updated navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -51,7 +51,7 @@ const NavBar = () => {
   return (
     <div
       ref={navContainerRef}
-      className="fixed inset-x-0 top-4 z-50 h-16 border-none rounded-md transition-all duration-700 sm:inset-x-6 max-w-screen-lg mx-auto"
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none rounded-md transition-all duration-700 sm:inset-x-6"
     >
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">

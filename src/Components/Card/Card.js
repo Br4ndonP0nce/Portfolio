e2ab6ae47b5dc148f846f2f7141e7f4adee644e4
Br4ndonP0nce <brandoncompany876@gmail.com>
2025-01-03 13:09:03 -0600

    updated social links

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -9,9 +9,9 @@ const Card = ({
   linkInfo = "View Project",
 }) => {
   return (
-    <div className="card card-compact bg-base-100 w-full sm:w-96 shadow-xl mx-auto bg-opacity-60">
-      <figure>
-        <img src={imgLink} alt="ProjectImage" />
+    <div className="card card-compact bg-base-100 w-full sm:w-96 shadow-xl mx-auto bg-opacity-60 min-h-[400px]">
+      <figure className="">
+        <img src={imgLink} alt="ProjectImage" className="w-2/3 py-2" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{projectName}</h2>
